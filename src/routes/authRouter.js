6b9f09eacbf926a77b0a523828a7ba61692d632c
Camilo Lira Sidou <camilo.sidou@semsa.am.br>
2020-07-23 16:51:52 -0400

    modificacoes 23/jul dicar

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
-const auth = require('../config/auth')()
+const auth = require('../config/auth')
 
 router.post('/signin', auth.signin)
+
+export default router
