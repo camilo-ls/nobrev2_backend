327816baac62e278840575f35207a88195744a51
Camilo Lira Sidou <cls@icomp.ufam.edu.br>
2021-02-11 10:24:49 -0400

    testing

--- src/controller/pactController.js
+++ src/controller/pactController.js
@@ -349,30 +349,30 @@ class pactController {
 
         const listaProfissionais = await db('profissionais').select()
 
-        for (let prof of listaProfissionais) {
-            console.log('>>>', listaProfissionais.indexOf(prof), 'de', listaProfissionais.length)
-            const listaProcedimentos = await db('pmp_padrao').select('COD_PROCED', 'QUANTIDADE').where({'CNES': prof.CNES, 'CBO': prof.CBO})
-            let coefPact = diasUteis/20
-            const jaPactuado = await db('pmp_hist').select('COEFICIENTE').where({'ANO': ano, 'MES': mes, 'VINC_ID': prof.VINC_ID}).first()
-            if (jaPactuado) {   
-                coefPact = jaPactuado.COEFICIENTE
-            }
-            else {
-                await db('pmp_hist').insert({'MES': mes, 'ANO': ano, 'CNES': prof.CNES, 'CNS': prof.CNS, 'VINC_ID': prof.VINC_ID, 'mat': 0, 'COEFICIENTE': coefPact, 'DIAS_PACTUADOS': diasUteis, 'FECHADO': 0, 'JUSTIFICATIVA': null})
-            }
+        // for (let prof of listaProfissionais) {
+        //     console.log('>>>', listaProfissionais.indexOf(prof), 'de', listaProfissionais.length)
+        //     const listaProcedimentos = await db('pmp_padrao').select('COD_PROCED', 'QUANTIDADE').where({'CNES': prof.CNES, 'CBO': prof.CBO})
+        //     let coefPact = diasUteis/20
+        //     const jaPactuado = await db('pmp_hist').select('COEFICIENTE').where({'ANO': ano, 'MES': mes, 'VINC_ID': prof.VINC_ID}).first()
+        //     if (jaPactuado) {   
+        //         coefPact = jaPactuado.COEFICIENTE
+        //     }
+        //     else {
+        //         await db('pmp_hist').insert({'MES': mes, 'ANO': ano, 'CNES': prof.CNES, 'CNS': prof.CNS, 'VINC_ID': prof.VINC_ID, 'mat': 0, 'COEFICIENTE': coefPact, 'DIAS_PACTUADOS': diasUteis, 'FECHADO': 0, 'JUSTIFICATIVA': null})
+        //     }
 
-            for (let proced of listaProcedimentos) {
-                await db('pmp_pactuados').insert({'MES': mes, 'ANO': ano, 'CNES': prof.CNES, 'INE': prof.INE, 'CNS': prof.CNS, 'VINC_ID': prof.VINC_ID, 'mat': 0, 'PROCEDIMENTO': proced.COD_PROCED, 'QUANTIDADE': Math.round(proced.QUANTIDADE * coefPact)})
-            }
-        }
+        //     for (let proced of listaProcedimentos) {
+        //         await db('pmp_pactuados').insert({'MES': mes, 'ANO': ano, 'CNES': prof.CNES, 'INE': prof.INE, 'CNS': prof.CNS, 'VINC_ID': prof.VINC_ID, 'mat': 0, 'PROCEDIMENTO': proced.COD_PROCED, 'QUANTIDADE': Math.round(proced.QUANTIDADE * coefPact)})
+        //     }
+        // }
 
-        console.log('> Deletando da pmp_hist os profissionais que saíram da base...')
-        const relVinculos = await db('profissionais').select('VINC_ID')
-        let listaVinculos = []
-        for (let vinculo of relVinculos) {
-            listaVinculos.push(vinculo.VINC_ID)
-        }
-        await db('pmp_hist').delete().whereNotIn('VINC_ID', listaVinculos).andWhere({'ANO': ano, 'MES': mes})
+        // console.log('> Deletando da pmp_hist os profissionais que saíram da base...')
+        // const relVinculos = await db('profissionais').select('VINC_ID')
+        // let listaVinculos = []
+        // for (let vinculo of relVinculos) {
+        //     listaVinculos.push(vinculo.VINC_ID)
+        // }
+        // await db('pmp_hist').delete().whereNotIn('VINC_ID', listaVinculos).andWhere({'ANO': ano, 'MES': mes})
 
         console.log('> Deletando da pmp_hist os profissionais com CBOs que não produzem meta...')
         const relCnes = await db('cnes').distinct('CNES')
@@ -385,7 +385,7 @@ class pactController {
             for (let cbo of relCBOs) {
                 listaCBOs.push(cbo.CBO)
             }
-            await db.delete().from('pmp_hist').leftJoin('profissionais', 'pmp_hist.VINC_ID', 'profissionais.VINC_ID').whereNotIn('profissionais.CBO', listaCBOs).andWhere({'CNES': cnes})
+            await db.delete().from('pmp_hist').leftJoin('profissionais', 'pmp_hist.VINC_ID', 'profissionais.VINC_ID').whereNotIn('CBO', listaCBOs).andWhere({'CNES': cnes})
         }
         const tempoFinal = new Date()
         const tempoPassado = (tempoFinal - tempoComeco)/1000
